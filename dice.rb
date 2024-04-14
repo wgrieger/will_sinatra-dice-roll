54cb92778bb1a562e8706b5d2473990b7aec6190
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-14 01:51:42 +0000

    first time rolling

--- dice.rb
+++ dice.rb
@@ -12,3 +12,26 @@ end
 get ("/wgrieger") do 
   "hello will, nice to see you here. I think tis works "
 end 
+
+get("/dice/2/6") do
+  first_die = rand(1..6)
+  second_die = rand(1..6)
+  sum = first_die + second_die
+	
+  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
+	
+  "<h1>2d6</h1>
+   <p>#{outcome}</p>"
+end
+
+#2x ten sides
+get ("/dice/2/10") do
+  first_die= rand(1...6)
+  second_die= rand(1...6)
+  sum= first_die + second_die
+
+  outcome= "you rolled #{first_die} and #{second_die} for total of #{sum}"
+
+  " <h1> Hello, world! </h1>
+  <p> #{outcome} </p>"
+end
