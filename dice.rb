87e4158f074353e7ae87cff537aabf28e939b43c
Will Grieger <165510301+wgrieger@users.noreply.github.com>
2024-04-14 19:45:46 +0000

    using_erb

--- dice.rb
+++ dice.rb
@@ -8,8 +8,7 @@ BetterErrors.application_root = __dir__
 BetterErrors::Middleware.allow_ip!('0.0.0.0/0.0.0.0')
 
 get ("/") do
-  '<h1> hello rice roll </h1>
-  <p><a href="https://glowing-giggle-pj76vq4p599gc55p-4567.app.github.dev/dice/2/10">2 times of 10 sided</a></p>'
+  erb(:elephant)
 end
 
 get ("/zebra") do
@@ -20,25 +19,41 @@ get ("/wgrieger") do
   "hello will, nice to see you here. Things are working"
 end 
 
-get("/dice/2/6") do
-  first_die = rand(1..6)
-  second_die = rand(1..6)
-  sum = first_die + second_die
-	
-  outcome = "You rolled a #{first_die} and a #{second_die} for a total of #{sum}."
-	
-  "<h1>2d6</h1>
-   <p>#{outcome}</p>"
-end
+
 
 #2x ten sides
 get ("/dice/2/10") do
-  first_die= rand(1...6)
-  second_die= rand(1...6)
+  first_die= rand(1...10)
+  second_die= rand(1...10)
   sum= first_die + second_die
 
   outcome= "you rolled #{first_die} and #{second_die} for total of #{sum}"
 
   " <h1> Hello, world! </h1>
   <p> #{outcome} </p>"
+end
+
+#2x six sides
+get ("/dice/2/6") do
+  first_die= rand(1...6)
+  second_die= rand(1...6)
+  sum= first_die + second_die
+
+  outcome= "you rolled #{first_die} and #{second_die} for total of #{sum}"
+
+  "<h1> 2x 6sides </h1>
+  <p> outcome </p>"
+
+end
+
+#1x 20 sides
+get ("/dice/1/20") do
+  first_die= rand(1...20)
+
+
+  outcome= "you rolled #{first_die} "
+  
+  "<h1> 1x 20 sides </h1>
+  <p> #{outcome} </p>"
+
 end
